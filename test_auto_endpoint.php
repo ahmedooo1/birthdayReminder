36b323c4d8b41c2fdbb506fb828734793d7a7a8c
AHMAD AHMAD <94185300+ahmedooo1@users.noreply.github.com>
2025-07-15 13:07:19 +0200

    Remove debug and test files to clean up the repository

--- test_auto_endpoint.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Test de l'endpoint auto_birthday_reminders.php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "Test de l'endpoint automatique...\n";
-$_GET['api_key'] = 'bd_12345_auto_reminder_secret_key_change_this';
-include 'api/auto_birthday_reminders.php';
-?>
